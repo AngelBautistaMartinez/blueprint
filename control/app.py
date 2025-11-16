ca86fcb3595e94e8bc4f286f5bfed88a937758bb
Angel Bautista Martinez <hectorhernendez095@gmail.com>
2025-11-15 20:03:14 -0800

    Update app.py

--- control/app.py
+++ control/app.py
@@ -35,4 +35,5 @@ def receive_data():
     return jsonify({"status": "success", "received": data})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
